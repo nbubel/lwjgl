e482875e16e072b70bd810d53cf6c2707fdc7376
pws <patrick.wilmes@bit-lake.com>
2016-04-02 20:59:35 +0200

    - entity base impl

--- game/src/main/java/com/bit/lake/lwjgl/entities/AbstractEntity.java
+++ game/src/main/java/com/bit/lake/lwjgl/entities/AbstractEntity.java
@@ -1,7 +1,49 @@
 package com.bit.lake.lwjgl.entities;
 
+import com.bit.lake.lwjgl.utils.InternalTextureLoader;
+import org.newdawn.slick.opengl.Texture;
+
+import static org.lwjgl.opengl.GL11.*;
 /**
  * @author Patrick Wilmes
  */
 public abstract class AbstractEntity implements Entity {
+    private float x, y;
+    private float width, height;
+    private Texture texture;
+
+    public AbstractEntity(final float x, final float y, EntityTextureName textureName) {
+        this.x = x;
+        this.y = y;
+        texture = InternalTextureLoader.loadTexture(textureName);
+    }
+
+    @Override
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    @Override
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    @Override
+    public float getWidth() {
+        return width;
+    }
+
+    @Override
+    public float getHeight() {
+        return height;
+    }
+
+    @Override
+    public void render() {
+        glBegin(GL_QUADS);
+        {
+
+        }
+        glEnd();
+    }
 }
---
+++ game/src/main/java/com/bit/lake/lwjgl/entities/EntityTextureName.java
@@ -0,0 +1,15 @@
+package com.bit.lake.lwjgl.entities;
+
+import com.bit.lake.lwjgl.utils.TextureName;
+
+/**
+ * @author Patrick Wilmes
+ */
+public enum EntityTextureName implements TextureName {
+    ;
+
+    @Override
+    public String getTextureName() {
+        return null;
+    }
+}
