a49bf4ffe69a930893f53a0dc08f1c178e6e1b7e
pws <patrick.wilmes@bit-lake.com>
2016-04-02 21:27:19 +0200

    - imports

--- game/src/main/java/com/bit/lake/lwjgl/environments/game/GameEnvironment.java
+++ game/src/main/java/com/bit/lake/lwjgl/environments/game/GameEnvironment.java
@@ -1,7 +1,6 @@
 package com.bit.lake.lwjgl.environments.game;
 
 import com.bit.lake.lwjgl.components.ComponentTextureName;
-import com.bit.lake.lwjgl.components.LocalizationKey;
 import com.bit.lake.lwjgl.components.TargetAction;
 import com.bit.lake.lwjgl.components.button.Button;
 import com.bit.lake.lwjgl.container.row.FlowLayoutContainer;
