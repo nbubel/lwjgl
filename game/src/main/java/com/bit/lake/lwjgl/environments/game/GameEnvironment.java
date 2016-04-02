a5653e86d6acbb3f827eab7fc76191455a51e148
pws <patrick.wilmes@bit-lake.com>
2016-04-02 17:47:08 +0200

    - game env template

---
+++ game/src/main/java/com/bit/lake/lwjgl/environments/game/GameEnvironment.java
@@ -0,0 +1,15 @@
+package com.bit.lake.lwjgl.environments.game;
+
+import com.bit.lake.lwjgl.environments.AbstractEnvironment;
+
+import java.util.Observable;
+
+/**
+ * @author Patrick Wilmes
+ */
+public class GameEnvironment extends AbstractEnvironment {
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
