9b24b0f97330507bc813ceb11ddbc985519cca0d
Jan Mischlich <jan.mischlich@accso.de>
2016-04-02 16:27:07 +0200

    - twitter4j added as dependency

---
+++ src/test/java/com/bit/lake/lwjgl/twitter/TwitterClientTest.java
@@ -0,0 +1,48 @@
+package com.bit.lake.lwjgl.twitter;
+
+import org.junit.Test;
+import twitter4j.*;
+
+public class TwitterClientTest {
+
+    @Test
+    public void readTwitterStream() throws Exception {
+        Twitter twitter = TwitterFactory.getSingleton();
+        //List<Status> statuses = twitter.getHomeTimeline();
+        QueryResult result = twitter.search(new Query("test"));
+
+        System.out.println("Showing home timeline.");
+        for (Status status : result.getTweets()) {
+            System.out.println(status.getUser().getName() + " : " +
+                    status.getUser().getStatusesCount() + ": " +
+                    status.getUser().getFollowersCount() + " : " +
+                    status.getUser().getListedCount() + " : " +
+                    status.getUser().getFavouritesCount() + " : " +
+                    status.getUser().getFriendsCount() + " : " +
+                    status.getFavoriteCount() + ": " +
+                    status.getRetweetCount() + " : " +
+                    getMentions(status.getUserMentionEntities()) + " : " +
+                    getHashtags(status.getHashtagEntities()) + " : " +
+                    status.getText());
+
+        }
+    }
+
+    private String getHashtags(HashtagEntity[] hashtagEntities) {
+        String ret = "";
+        for (HashtagEntity entity :
+                hashtagEntities) {
+            ret += entity.getText() + " : ";
+        }
+        return ret;
+    }
+
+    private String getMentions(UserMentionEntity[] userMentionEntities) {
+        String ret = "";
+        for (UserMentionEntity entity :
+                userMentionEntities) {
+            ret += entity.getName();
+        }
+        return ret;
+    }
+}
