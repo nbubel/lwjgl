25968b5ee81f4f763edf9f363f93c061eb26ef41
pws <patrick.wilmes@bit-lake.com>
2016-04-02 20:55:38 +0200

    - extended entity interface

--- game/src/main/java/com/bit/lake/lwjgl/entities/Entity.java
+++ game/src/main/java/com/bit/lake/lwjgl/entities/Entity.java
@@ -4,5 +4,11 @@ package com.bit.lake.lwjgl.entities;
  * @author Patrick Wilmes
  */
 public interface Entity {
+    void setX(float x);
+    void setY(float y);
+    float getX();
+    float getY();
+    float getWidth();
+    float getHeight();
     void render();
 }
