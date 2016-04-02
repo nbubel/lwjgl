bbeac72fd7fe2b8f0a95541cc23fe43f598a8e63
pws <patrick.wilmes@bit-lake.com>
2016-04-02 16:36:32 +0200

    - target action for button

--- src/main/java/com/bit/lake/lwjgl/components/button/Button.java
+++ src/main/java/com/bit/lake/lwjgl/components/button/Button.java
@@ -3,6 +3,7 @@ package com.bit.lake.lwjgl.components.button;
 import com.bit.lake.lwjgl.components.AbstractComponent;
 import com.bit.lake.lwjgl.components.ComponentTextureName;
 import com.bit.lake.lwjgl.components.LocalizationKey;
+import com.bit.lake.lwjgl.components.TargetAction;
 import com.bit.lake.lwjgl.utils.InternalTextureLoader;
 import com.bit.lake.lwjgl.utils.MouseButton;
 import org.apache.log4j.Logger;
@@ -15,8 +16,8 @@ public class Button extends AbstractComponent {
 
     private static final Logger LOGGER = Logger.getLogger(Button.class);
 
-    public Button(float x, float y) {
-        super(x, y, InternalTextureLoader.loadTexture(ComponentTextureName.menuButton), LocalizationKey.btnMenuText);
+    public Button(float x, float y, final TargetAction targetAction) {
+        super(x, y, InternalTextureLoader.loadTexture(ComponentTextureName.menuButton), LocalizationKey.btnMenuText, targetAction);
     }
 
     @Override
