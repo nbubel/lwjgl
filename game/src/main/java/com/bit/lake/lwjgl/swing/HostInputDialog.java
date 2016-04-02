f5b6a82e4a332378deab44ad6148becf8b5d7d72
pws <patrick.wilmes@bit-lake.com>
2016-04-02 23:39:14 +0200

    - dialog impl

--- game/src/main/java/com/bit/lake/lwjgl/swing/HostInputDialog.java
+++ game/src/main/java/com/bit/lake/lwjgl/swing/HostInputDialog.java
@@ -10,6 +10,9 @@ import java.awt.event.ActionListener;
  */
 public class HostInputDialog extends JFrame implements ActionListener {
 
+    private String hostIp;
+    private JTextField textField = new JTextField();
+
     public HostInputDialog() {
         setTitle("LWJGL Game Host Input Dialog");
         setLocationRelativeTo(null);
@@ -21,14 +24,25 @@ public class HostInputDialog extends JFrame implements ActionListener {
     }
 
     private void initializeDialog() {
-        JTextField textField = new JTextField();
         textField.setLocation(10, 10);
         textField.setSize(new Dimension(100, 20));
         add(textField);
+
+        JButton button = new JButton("OK");
+        button.setSize(100, 20);
+        button.setLocation(120, 20);
+        button.addActionListener(this);
+        add(button);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("OK")) {
+            hostIp = textField.getText();
+        }
+    }
 
+    public String getHostIp() {
+        return hostIp;
     }
 }
