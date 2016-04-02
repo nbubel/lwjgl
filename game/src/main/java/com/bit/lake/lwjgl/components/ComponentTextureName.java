f36d41f2cb3dfbdac20f5c3ccb20c64f8ec239c6
pws <patrick.wilmes@bit-lake.com>
2016-04-02 23:52:58 +0200

    - game background

--- game/src/main/java/com/bit/lake/lwjgl/components/ComponentTextureName.java
+++ game/src/main/java/com/bit/lake/lwjgl/components/ComponentTextureName.java
@@ -6,8 +6,7 @@ import com.bit.lake.lwjgl.utils.TextureName;
  * @author Patrick Wilmes
  */
 public enum ComponentTextureName implements TextureName {
-    menuButton("textures/start/play.png"),
-    gameBackground("textures/grid/bg.png");
+    menuButton("textures/start/play.png");
 
     private String textureName;
 
--- game/src/main/java/com/bit/lake/lwjgl/environments/game/GameEnvironment.java
+++ game/src/main/java/com/bit/lake/lwjgl/environments/game/GameEnvironment.java
@@ -4,9 +4,13 @@ import com.bit.lake.lwjgl.components.ComponentTextureName;
 import com.bit.lake.lwjgl.components.TargetAction;
 import com.bit.lake.lwjgl.components.button.Button;
 import com.bit.lake.lwjgl.container.row.GridLayoutContainer;
+import com.bit.lake.lwjgl.entities.Entity;
+import com.bit.lake.lwjgl.entities.EntityTextureName;
+import com.bit.lake.lwjgl.entities.background.BackgroundEntity;
 import com.bit.lake.lwjgl.environments.AbstractEnvironment;
 import com.bit.lake.lwjgl.environments.Environment;
 import com.bit.lake.lwjgl.game.GameController;
+import com.bit.lake.lwjgl.utils.Timer;
 
 import java.util.Observable;
 
@@ -17,8 +21,9 @@ public class GameEnvironment extends AbstractEnvironment {
 
     private static GameEnvironment instance;
     private GameController controller;
-
+    private Entity background;
     private GameEnvironment() {
+        background = new BackgroundEntity(0, 0, EntityTextureName.gameBackground);
         GridLayoutContainer container = new GridLayoutContainer();
 
         for (int y = 0; y < container.rowDefMap.size(); y++) {
@@ -45,5 +50,11 @@ public class GameEnvironment extends AbstractEnvironment {
     @Override
     public void update(Observable o, Object arg) {
 
+    }
+
+    @Override
+    public void render(Timer timer) {
+        background.render();
+        super.render(timer);
     }
 }
